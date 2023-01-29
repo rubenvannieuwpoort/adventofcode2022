9b2d0df2fd6688ee135ee6d40ffa98f33bbc1ecc
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2023-01-29 09:42:31 +0100

    Add solutions for day 12

--- python/day12/b.py
+++ python/day12/b.py
@@ -6,37 +6,39 @@ height = 41
 hmap = [[0 for _ in range(0, width)] for _ in range(0, height)]
 turn = [[10000 for _ in range(0, width)] for _ in range(0, height)]
 
-start = None
+possible_start_positions = []
 end = None
 
 for y, line in enumerate(lines):
     for x, c in enumerate(line):
         if c == 'S':
-            assert start == None
             c = 'a'
-            start = (x, y)
         elif c == 'E':
             assert end == None
             c = 'z'
             end = (x, y)
+
+        if c == 'a':
+            possible_start_positions.append((x, y))
+
         hmap[y][x] = ord(c) - ord('a')
 
-turn[start[1]][start[0]] = 0
+turn[end[1]][end[0]] = 0
 
-while turn[end[1]][end[0]] == 10000:
+while all(map(lambda x: turn[x[1]][x[0]] == 10000, possible_start_positions)):
     for y in range(0, height):
         for x in range(0, width):
             values = [turn[y][x]]
 
-            if x > 0 and hmap[y][x] <= hmap[y][x - 1] + 1:
+            if x > 0 and hmap[y][x - 1] <= hmap[y][x] + 1:
                 values.append(turn[y][x - 1] + 1)
-            if x < width - 1 and hmap[y][x] <= hmap[y][x + 1] + 1:
+            if x < width - 1 and hmap[y][x + 1] <= hmap[y][x] + 1:
                 values.append(turn[y][x + 1] + 1)
-            if y > 0 and hmap[y][x] <= hmap[y - 1][x] + 1:
+            if y > 0 and hmap[y - 1][x] <= hmap[y][x] + 1:
                 values.append(turn[y - 1][x] + 1)
-            if y < height - 1 and hmap[y][x] <= hmap[y + 1][x] + 1:
+            if y < height - 1 and hmap[y + 1][x] <= hmap[y][x] + 1:
                 values.append(turn[y + 1][x] + 1)
 
             turn[y][x] = min(values)
 
-print(turn[end[1]][end[0]])
+print(min(map(lambda x: turn[x[1]][x[0]], possible_start_positions)))
