16f26815857cc5897dc287338ec6419940aa4d6e
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2023-02-06 12:07:02 +0100

    Add second part of day 16

--- python/day16/b.py
+++ python/day16/b.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass, field
 from typing import Any
 
 
-total_minutes = 30
+total_minutes = 26
 
 class PriorityQueue:
     def __init__(self):
@@ -24,15 +24,19 @@ class State:
     max_score: int
     score: int = field(compare=False)
     closed_valves: list[str] = field(compare=False)
-    current_position: str = field(compare=False)
-    current_minute: int = field(compare=False)
+    player_position: str = field(compare=False)
+    player_free: int = field(compare = False)
+    elephant_position: str = field(compare=False)
+    elephant_free: int = field(compare = False)
 
-    def __init__(self, score, closed_valves, current_position, current_minute):
-        self.max_score = State.compute_max_score(score, closed_valves, current_minute)
+    def __init__(self, score, closed_valves, player_position, player_free, elephant_position, elephant_free):
+        self.max_score = State.compute_max_score(score, closed_valves, min(player_free, elephant_free))
         self.score = score
         self.closed_valves = closed_valves
-        self.current_position = current_position
-        self.current_minute = current_minute
+        self.player_position = player_position
+        self.player_free = player_free
+        self.elephant_position = elephant_position
+        self.elephant_free = elephant_free
 
     def compute_max_score(score, closed_valves, current_minute):
         global nodes, total_minutes
@@ -53,6 +57,7 @@ for line in lines:
     neighbours[name] = nbs
 
 
+# calculate distances from every valve to every valve (with pressure > 0)
 distances = { node: {} for node in nodes }
 for node in nodes:
     pq = PriorityQueue()
@@ -76,7 +81,7 @@ closed_valves = [ n for n in nodes if nodes[n] > 0 ]
 closed_valves.sort(key=lambda x: nodes[x], reverse=True)
 
 pq = PriorityQueue()
-pq.insert(State(0, closed_valves, 'AA', 1))
+pq.insert(State(0, closed_valves, 'AA', 1, 'AA', 1))
 
 best_score = 0
 while True:
@@ -88,15 +93,32 @@ while True:
     if c.score > best_score:
         best_score = c.score
 
-    if c.current_minute == total_minutes or not c.closed_valves or not neighbours[c.current_position]:
+    current_minute = min(c.player_free, c.elephant_free)
+
+    if current_minute == total_minutes or not c.closed_valves or (not neighbours[c.player_position] and not neighbours[c.elephant_position]):
         continue
 
-    for nb in c.closed_valves:
-        if nb in distances[c.current_position]:
-            cv = c.closed_valves.copy()
-            dis = distances[c.current_position][nb]
-            cv.remove(nb)
-            score = c.score + (nodes[nb] * (total_minutes - c.current_minute - dis))
-            pq.insert(State(score, cv, nb, c.current_minute + dis + 1))
+    # this is not completely foolproof as there is no option to wait, but it works for the input
+    # (theoretically for the last step it might be faster to wait for the other to finish and handle last valve
+    # than to start going there yourself, but this doesn't seem to happen in the input, so I will not fix this)
+    if c.player_free == current_minute:
+        for nb in c.closed_valves:
+            if nb in distances[c.player_position]:
+                cv = c.closed_valves.copy()
+                dis = distances[c.player_position][nb]
+                cv.remove(nb)
+                score = c.score + (nodes[nb] * (total_minutes - current_minute - dis))
+                pq.insert(State(score, cv, nb, current_minute + dis + 1, c.elephant_position, c.elephant_free))
+    elif c.elephant_free == current_minute:
+        for nb in c.closed_valves:
+            if nb in distances[c.elephant_position]:
+                cv = c.closed_valves.copy()
+                dis = distances[c.elephant_position][nb]
+                cv.remove(nb)
+                score = c.score + (nodes[nb] * (total_minutes - current_minute - dis))
+                pq.insert(State(score, cv, c.player_position, c.player_free, nb, current_minute + dis + 1))
+    else:
+        raise Exception('This should never happen')
 
 print(best_score)
+
