500ca6ac9591f3028218d760673e1b7c8fd977ad
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2023-02-09 11:18:51 +0100

    Add day 21 first part

--- python/day21/a.py
+++ python/day21/a.py
@@ -1,1 +1,26 @@
+def parseexpr(str):
+    global monkeys
+    if str.isnumeric():
+        return lambda: int(str)
+
+    parts = str.split(' ')
+    assert len(parts) == 3
+    assert parts[1] in ['+', '-', '*', '/']
+    if parts[1] == '+':
+        return lambda: monkeys[parts[0]]() + monkeys[parts[2]]()
+    if parts[1] == '-':
+        return lambda: monkeys[parts[0]]() - monkeys[parts[2]]()
+    if parts[1] == '*':
+        return lambda: monkeys[parts[0]]() * monkeys[parts[2]]()
+    if parts[1] == '/':
+        return lambda: monkeys[parts[0]]() // monkeys[parts[2]]()
+
 lines = list(map(lambda x: x.rstrip(), open('input.txt', 'r').readlines()))
+
+monkeys = {}
+for line in lines:
+    parts = line.split(': ')
+    monkeyname = parts[0]
+    monkeys[monkeyname] = parseexpr(parts[1])
+
+print(monkeys['root']())
