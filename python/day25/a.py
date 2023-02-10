238731bbfd338ca7554f4375f928995b4a14605f
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2023-02-10 20:28:59 +0100

    Add day 25 first part

--- python/day25/a.py
+++ python/day25/a.py
@@ -1,1 +1,17 @@
+def from_snafu(x):
+    return sum(map(lambda x: 5**x[0] * {'=': -2, '-': -1, '0': 0, '1': 1, '2': 2}[x[1]], enumerate(reversed(x))))
+
+def to_snafu(x):
+    result = ''
+    while x:
+        digit = x % 5
+        if digit > 2:
+            digit -= 5
+        x -= digit
+        x = x // 5
+        result += '=-012'[digit + 2]
+    return result[::-1]
+
 lines = list(map(lambda x: x.rstrip(), open('input.txt', 'r').readlines()))
+
+print(to_snafu(sum(map(lambda x: from_snafu(x), lines))))
